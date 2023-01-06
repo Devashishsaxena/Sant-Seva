0145aa4d8516b0ad74a73c639deeb9d328dfe1d3
krishnapal <kps42891@gmail.com>
2023-01-06 18:44:32 +0530

    added navbar and css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,139 @@
-import React from "react";
-
+import React, { useState } from 'react'
+import './Home.css';
+import Sant1 from '../Assests/Img/Sant1.jpg';
+import Sant2 from '../Assests/Img/Sant2.jpg';
+import Sant3 from '../Assests/Img/Sant3.jpg';
+import { FaGreaterThan } from 'react-icons/fa'
 function Home() {
-    return (<>
-        <h3 className="text-center">Home Page</h3>
-    </>)
+    return (
+        <React.Fragment>
+            <div className='container'>
+                <div className='row'>
+                    <div className='mt-5'>
+                        <h1>
+                            About Us
+                        </h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse quisquam beatae, debitis nisi quod nam architecto unde non, quibusdam in corporis corrupti. Nostrum, accusamus adipisci voluptates facilis dolorum velit tempora molestias voluptatibus laborum beatae, necessitatibus dolores, tempore assumenda iste quo quis nesciunt. Exercitationem voluptas eligendi consequuntur iusto sint at harum eum. Explicabo quisquam aliquam non quis assumenda, sint quos iusto, neque laboriosam quo minima aut amet culpa, ex dolore quidem accusantium odit. Quaerat suscipit distinctio fugiat repellendus ea reiciendis quisquam, quo odio sunt dignissimos aperiam nihil ipsa nesciunt perspiciatis eos facilis minus. Eius delectus exercitationem voluptatem repellendus accusamus laboriosam consectetur.
+                        </p>
+                        <div className='d-flex flex-direction-column'>
+                            <div className='col-6'>
+                                <ul>
+                                    <li className='breadcrumb'>
+                                        <span>{<FaGreaterThan className='FaGreaterThan ' />}  Certified Professionals </span>
+                                    </li>
+                                    <li className='breadcrumb mt-2'>
+                                        <span>{<FaGreaterThan className='FaGreaterThan' />} Former Chief Executives</span>
+                                    </li>
+                                    <li className='breadcrumb mt-2'>
+                                        <span>{<FaGreaterThan className='FaGreaterThan' />} Real Estate Professionals </span>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <div className='col-6'>
+                                <ul>
+                                    <li className='breadcrumb'>
+
+                                        <span>{<FaGreaterThan className='FaGreaterThan' />} Nobel Laureate Economists </span>
+                                    </li>
+                                    <li className='breadcrumb mt-2'>
+                                        <span>{<FaGreaterThan className='FaGreaterThan' />} Former Political Leaders</span>
+                                    </li>
+                                    <li className='breadcrumb mt-2'>
+                                        <span>{<FaGreaterThan className='FaGreaterThan' />} Chartered Financial Analysts</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className='d-flex justify-content-center align-items-center mt-5'>
+                <button><a href='/about'>Read More</a></button>
+            </div>
+            <div className='container-fluid' style={{backgroundColor:"#EEF4F2"}}>
+                    <div className='mt-5'>
+                        <h1>Ashram List</h1>
+                </div>
+                <div className="row d-flex justify-content-evenly mt-5">
+                    <div className="col-lg-4 col-md-12 col-sm-12  mt-2">
+                        <div className="row d-flex justify-content-center aligns-center">
+                            <div className="col-lg-3 col-md-4 col-sm-12 ourSanta">
+                                <img src={Sant1} />
+                            </div>
+                            <div className="col-lg-7 col-md-4 col-sm-12 text-end mt-2">
+                                <h4>Corporate Finance</h4>
+                                <p>Lorem ipsum, dolor sit amet</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12  mt-2">
+                        <div className="row d-flex justify-content-center aligns-center">
+                            <div className="col-lg-3 col-md-4 col-sm-12 ourSanta">
+                                <img src={Sant2}  />
+                            </div>
+                            <div className="col-lg-7 col-md-4 col-sm-12 text-end mt-2">
+                                <h4>Corp Restructuring</h4>
+                                <p>Lorem ipsum, dolor sit amet <br />consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12  mt-2">
+                        <div className="row d-flex justify-content-center aligns-center">
+                            <div className="col-lg-3 col-md-4 col-sm-12 ourSanta">
+                                <img src={Sant3}  />
+                            </div>
+                            <div className="col-lg-7 col-md-4 col-sm-12 text-end mt-2">
+                                <h4>Economic Consulting</h4>
+                                <p>Lorem ipsum, dolor sit amet <br />consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row d-flex justify-content-evenly mt-3">
+                    <div className="col-lg-4 col-md-12 col-sm-12 ">
+                        <div className="row d-flex justify-content-center aligns-center">
+                            <div className="col-lg-3 col-md-4 col-sm-12 ourSanta">
+                                <img src={Sant1}  />
+                            </div>
+                            <div className="col-lg-7 col-md-4 col-sm-12 text-end mt-2">
+                                <h4>Litigation Consulting</h4>
+                                <p>Lorem ipsum, dolor sit amet <br />consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12 ">
+                        <div className="row d-flex justify-content-center aligns-center">
+                            <div className="col-lg-3 col-md-4 col-sm-12 ourSanta">
+                                <img src={Sant2}  />
+                            </div>
+                            <div className="col-lg-7 col-md-4 col-sm-12 text-end mt-2">
+                                <h4>Strategic Consulting</h4>
+                                <p>Lorem ipsum, dolor sit amet <br />consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12 ">
+                        <div className="row d-flex justify-content-center aligns-center">
+                            <div className="col-lg-3 col-md-4 col-sm-12 ourSanta">
+                                <img src={Sant3}  />
+                            </div>
+                            <div className="col-lg-7 col-md-4 col-sm-12 text-end mt-2">
+                                <h4>Tech Finance</h4>
+                                <p>Lorem ipsum, dolor sit amet <br />consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+                <div className='d-flex justify-content-center align-items-center mt-5'>
+                    <button><a href='/about'>View All</a></button>
+                </div>
+            </div>
+        </React.Fragment>
+        
+    )
 }
 export default Home;
--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -1,29 +1,84 @@
-import React from 'react';
-// import { Navbar } from 'react-bootstrap';
+// import React from 'react';
+// // import { Navbar } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+// import Carousel from 'react-bootstrap/Carousel';
+// // import Sant1 from '../Navbar/Img/Sant1.jpg';
+// // import Sant2 from '../Navbar/Img/Sant2.jpg';
+// // import Sant3 from '../Navbar/Img/Sant3.jpg';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Button from 'react-bootstrap/Button';
+
+// function NavBar() {
+//     return (
+//              <React.Fragment>
+//              <Navbar bg="dark" expand="lg" className="sticky-top">
+//                  <Container >
+//                      <Navbar.Brand to="/home" style={{ color: 'white', margin: '5px', textDecoration: 'none' }} className="">Sant Seva</Navbar.Brand>
+//                      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                      <Navbar.Collapse id="basic-navbar-nav">
+//                          <Nav className="ms-auto">
+//                              <Link to={"/home"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Home</Link>
+//                              <Link to={"/about"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>About Us</Link>
+//                              <Link to={"/aasharam "} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Aasharam List</Link>
+//                              <Link to={"/oursant"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}> OurSant</Link>
+//                              <Link to={"/testinomials"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}> Testinomials</Link>
+//                              <Link to={"/contact"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Contact</Link>
+//                              <Link to={"/blog"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Blog</Link>
+//                              <Button to="#donate" style={{ color: 'white', margin: '5px', textDecoration: 'none' }}>Donate</Button>
+                             
+//                          </Nav>
+                         
+//                      </Navbar.Collapse>
+//                  </Container>
+                
+//              </Navbar>
+           
+//          </React.Fragment>        
+//     )
+// }
+// export default NavBar;
+
+
+
+
+
+
+
+
+import React,{useState} from 'react';
+import '../Navbar/NavBar.css';
+import { FaBars, FaTimes } from 'react-icons/fa'
+import Sant1 from '../Assests/Img/Sant1.jpg';
 import { Link } from 'react-router-dom';
-import Carousel from 'react-bootstrap/Carousel';
-import Sant1 from '../Navbar/Img/Sant1.jpg';
-import Sant2 from '../Navbar/Img/Sant2.jpg';
-import Sant3 from '../Navbar/Img/Sant3.jpg';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 function NavBar() {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMenu = () => setClick(false)
+    const [color, setColor] = useState(false)
+    const changeColor = () => {
+        if (window.scrollY >= 90) {
+            setColor(true)
+        }
+        else {
+            setColor(false)
+        }
+    }
+    window.addEventListener('scroll', changeColor)
     return (
-    
-           
-
-       
-             <React.Fragment>
-             <Navbar bg="dark" expand="lg" className="sticky-top">
-                 <Container >
-                     <Navbar.Brand to="/home" style={{ color: 'white', margin: '5px', textDecoration: 'none' }} className="">Sant Seva</Navbar.Brand>
-                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                     <Navbar.Collapse id="basic-navbar-nav">
-                         <Nav className="ms-auto">
-                             <Link to={"/home"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Home</Link>
+        <React.Fragment>
+           <div className={color?'header header-bg': "header"}>
+           <nav className='navbar'>
+                        <h5 className='text-white'>Sant Seva</h5>
+               <div className='hamburger' onClick={handleClick}>
+                  {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
+                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                                                <Link to={"/home"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Home</Link>
                              <Link to={"/about"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>About Us</Link>
                              <Link to={"/aasharam "} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Aasharam List</Link>
                              <Link to={"/oursant"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}> OurSant</Link>
@@ -31,21 +86,11 @@ function NavBar() {
                              <Link to={"/contact"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Contact</Link>
                              <Link to={"/blog"} style={{ color: 'white', margin: '10px', textDecoration: 'none' }}>Blog</Link>
                              <Button to="#donate" style={{ color: 'white', margin: '5px', textDecoration: 'none' }}>Donate</Button>
-                             
-                         </Nav>
-                         
-                     </Navbar.Collapse>
-                 </Container>
-                
-             </Navbar>
-           
-            
-         </React.Fragment>
-           
-          
+                </ul>
+            </nav>
+        </div>
+        </React.Fragment>
 
-        
-         
     )
 }
-export default NavBar;
+export default NavBar
